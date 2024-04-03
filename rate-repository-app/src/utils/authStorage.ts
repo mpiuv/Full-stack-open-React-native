15a2043db982818fc3fdfc57d6db3ca6bb147b20
T. L <43107946+mpiuv@users.noreply.github.com>
2024-04-03 13:15:54 +0300

    10,14

---
+++ rate-repository-app/src/utils/authStorage.ts
@@ -0,0 +1,29 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+class AuthStorage {
+  constructor(namespace = 'auth') {
+    this.namespace = namespace;
+  }
+
+  getAccessToken() {
+    // Get the access token for the storage
+    const accessToken = await AsyncStorage.getItem(
+        `${this.namespace}:accessToken`,
+      );
+    return accessToken? JSON.parse(accessToken):null;
+  }
+
+  async setAccessToken(accessToken:any) {
+    // Add the access token to the storage
+    await AsyncStorage.setItem(
+      `${this.namespace}:accessToken`,
+      JSON.stringify(accessToken),
+    );
+  }
+
+  async removeAccessToken() {
+    await AsyncStorage.removeItem(`${this.namespace}:accessToken`);
+  }
+}
+
+export default AuthStorage;
